5065a97e8ac9bfe7ddef0e1856c0fe4d394b28cb
FutbalaMato <Buftbala@gmail.com>
2021-12-03 09:35:43 +0100

    Add project files.

---
+++ Open-Lab-10.01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Open_Lab_10._01
+{
+    class Program
+    {
+        class Book
+        {
+            public string title;
+            private int pages;
+        }
+        static void Main(string[] args)
+        {
+            Book LOTR = new Book();
+
+            string title = "Samova cesta";
+
+            Console.WriteLine(title);
+        }
+    }
+}
